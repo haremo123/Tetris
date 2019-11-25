6c23ea691b674c56e2b3b9dda6d45d79909d1f76
haremo <haremo@haremo-PC>
2019-11-25 22:44:19 +0900

    test

--- Tetrs/src/application/Controller.java
+++ Tetrs/src/application/Controller.java
@@ -7,10 +7,10 @@ public class Controller {
 	public static int  XMAX = Tetris.XMAX;
 	public static int YMAX = Tetris.YMAX;
 	public static int [][] MESH = Tetris.MESH;
-	
+
 	// テトリミノの動き 右回転
 	public static void MoveRight (Form form) {
-		if(form.a.getX() + MOVE <= XMAX - SIZE && form.b.getX() + MOVE <= XMAX - SIZE 
+		if(form.a.getX() + MOVE <= XMAX - SIZE && form.b.getX() + MOVE <= XMAX - SIZE
 				&& form.c.getX() + MOVE <= XMAX - SIZE && form.d.getX() + MOVE <= XMAX -SIZE) {
 		int movea = MESH[((int) form.a.getX() / SIZE) + 1][((int) form.a.getY() / SIZE)];
 		int moveb = MESH[((int) form.b.getX() / SIZE) + 1][((int) form.b.getY() / SIZE)];
@@ -22,13 +22,13 @@ public class Controller {
 			form.c.setX(form.c.getX() + MOVE);
 			form.d.setX(form.d.getX() + MOVE);
 		}
-			
+
 		}
 	}
-	
+
 	// 左回転
 	public static void MoveLeft (Form form) {
-		if(form.a.getX() - MOVE >= 0 && form.b.getX() + MOVE - MOVE >= 0 
+		if(form.a.getX() - MOVE >= 0 && form.b.getX() + MOVE - MOVE >= 0
 				&& form.c.getX() - MOVE >= 0 && form.d.getX() - MOVE >= 0) {
 		int movea = MESH[((int) form.a.getX() / SIZE) - 1][((int) form.a.getY() / SIZE)];
 		int moveb = MESH[((int) form.b.getX() / SIZE) - 1][((int) form.b.getY() / SIZE)];
@@ -39,9 +39,11 @@ public class Controller {
 			form.b.setX(form.b.getX() - MOVE);
 			form.c.setX(form.c.getX() - MOVE);
 			form.d.setX(form.d.getX() - MOVE);
+
+			form
 		}
-			
+
 		}
 	}
-	
+
 }
