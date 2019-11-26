6036806d46a4851f4b1eb99ac7e451565773c26e
sotuken <sotuken@C30321>
2019-11-26 14:34:09 +0900

    途中

--- Tetrs/src/application/Tetris.java
+++ Tetrs/src/application/Tetris.java
@@ -1,10 +1,20 @@
 package application;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-
 
 public class Tetris extends Application{
 
@@ -21,13 +31,23 @@ public class Tetris extends Application{
 		private static boolean game = true;
 		private static Form nextObj= controller.makeRect();
 		private static int LinesNo = 0;
-		
-		
-		
-	@Override
+
+		//
+		public void main(String[] args) {launch(args);}
+		@Override
 	public void start(Stage arg0) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
-		
+		  for(int[] a: MESH) {
+		    Arrays.fill(a, 0);
+		  }
+
+		  //
+		  Line line = new Line(XMAX,0,XMAX,YMAX);
+		  Text scoretext = new Text("Score: ");
+		  scoretext.setStyle(" -fx-font: 20 arials;");
+		  scoretext.setY(50);
+		  scoretext.setX(XMAX + 5);
+		  Text level = new Text("Line:");
+
 	}
 
 }
