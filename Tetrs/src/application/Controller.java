0988c18fe8567f3eed58eab0544b55e84a180926
sotuken <sotuken@C30321>
2019-11-26 09:56:37 +0900

    Controller完成

--- Tetrs/src/application/Controller.java
+++ Tetrs/src/application/Controller.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.scene.shape.Rectangle;
+
 public class Controller {
 	//　テトリスクラスから変数など取得
 	public static final int MOVE = Tetris.MOVE;
@@ -16,7 +18,7 @@ public class Controller {
 		int moveb = MESH[((int) form.b.getX() / SIZE) + 1][((int) form.b.getY() / SIZE)];
 		int movec = MESH[((int) form.c.getX() / SIZE) + 1][((int) form.c.getY() / SIZE)];
 		int moved = MESH[((int) form.d.getX() / SIZE) + 1][((int) form.d.getY() / SIZE)];
-		if(movea = 0 && movea == moveb == movec && movec && movec == moved) {
+		if(movea == 0 && movea == moveb && moveb == movec && movec == moved) {
 			form.a.setX(form.a.getX() + MOVE);
 			form.b.setX(form.b.getX() + MOVE);
 			form.c.setX(form.c.getX() + MOVE);
@@ -34,16 +36,92 @@ public class Controller {
 		int moveb = MESH[((int) form.b.getX() / SIZE) - 1][((int) form.b.getY() / SIZE)];
 		int movec = MESH[((int) form.c.getX() / SIZE) - 1][((int) form.c.getY() / SIZE)];
 		int moved = MESH[((int) form.d.getX() / SIZE) - 1][((int) form.d.getY() / SIZE)];
-		if(movea = 0 && movea == moveb == movec && movec && movec == moved) {
+		if(movea == 0 && movea == moveb && moveb == movec && movec == moved) {
 			form.a.setX(form.a.getX() - MOVE);
 			form.b.setX(form.b.getX() - MOVE);
 			form.c.setX(form.c.getX() - MOVE);
 			form.d.setX(form.d.getX() - MOVE);
-
-			form
 		}
 
 		}
+
 	}
 
-}
+		// テトリミノの作成
+		public static Form makeRect() {
+
+		  int block = (int)(Math.random() * 100);
+		  String name;
+
+		  Rectangle a = new Rectangle(SIZE-1,SIZE-1),
+		      b = new Rectangle(SIZE-1,SIZE-1),
+		      c = new Rectangle(SIZE-1,SIZE-1),
+		      d = new Rectangle(SIZE-1,SIZE-1);
+
+		  if(block < 15) {
+		    a.setX(XMAX / 2 - SIZE);
+		    b.setX(XMAX / 2 - SIZE);
+		    b.setY(SIZE);
+		    c.setX(XMAX / 2);
+		    c.setY(SIZE);
+		    d.setX(XMAX / 2 + SIZE);
+		    d.setY(SIZE);
+		    name = "j";
+
+		  }else if (block < 30) {
+		    a.setX(XMAX / 2 + SIZE);
+        b.setX(XMAX / 2 - SIZE);
+        b.setY(SIZE);
+        c.setX(XMAX / 2);
+        c.setY(SIZE);
+        d.setX(XMAX / 2 + SIZE);
+        d.setY(SIZE);
+        name = "l";
+
+		  }else if(block < 45) {
+		    a.setX(XMAX / 2 - SIZE);
+        b.setX(XMAX / 2);
+        b.setX(XMAX / 2 - SIZE);
+        c.setY(SIZE);
+        d.setX(XMAX / 2);
+        d.setY(SIZE);
+        name = "o";
+
+		  }else if(block < 60) {
+		  a.setX(XMAX / 2 + SIZE);
+      b.setX(XMAX / 2);
+      b.setX(XMAX / 2);
+      c.setY(SIZE);
+      d.setX(XMAX / 2 - SIZE);
+      d.setY(SIZE);
+      name = "s";
+
+		  }else if(block < 75) {
+		    a.setX(XMAX / 2 - SIZE);
+        b.setX(XMAX / 2);
+        b.setX(XMAX / 2);
+        c.setY(SIZE);
+        d.setX(XMAX / 2 + SIZE);
+        name = "t";
+
+		  }else if(block < 90) {
+        a.setX(XMAX / 2 - SIZE);
+        b.setX(XMAX / 2);
+        b.setX(XMAX / 2 + SIZE);
+        c.setY(SIZE);
+        d.setX(XMAX / 2 + SIZE + SIZE);
+        d.setY(SIZE);
+        name = "z";
+
+		  }else {
+        a.setX(XMAX / 2 - SIZE - SIZE);
+        b.setX(XMAX / 2 - SIZE);
+        b.setX(XMAX / 2);
+        c.setY(SIZE);
+        d.setX(XMAX / 2 + SIZE);
+        name = "i";
+		  }
+
+		  return new Form (a,b,c,d,name);
+		}
+	}
